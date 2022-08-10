e3ac1c34ec00d39e4ae4a57253a1b88145125b7d
Aryan Jassal <aryanjassal.au@gmail.com>
2022-08-10 14:44:33 +1000

    added custom charset option

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,39 @@
 use std::{env, fs};
 
-const CHARACTERS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_ ";
-
 fn main() {
+    let base_charset: &String = &"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_<>/[]{}+=., ".to_string();
     let args: Vec<String> = env::args().collect();
     let mode = &args[1];
     let filename = &args[2];
     let rotation = &args[3].parse().unwrap_or(1);
+    let charset = &args.get(4).unwrap_or(base_charset);
     let file_content = fs::read_to_string(filename).unwrap();
 
     println!();
     if mode == "encrypt" || mode == "e" {
-        println!("{}", cypher_encode(&file_content, *rotation));    
+        println!("{}", cypher_encode(&file_content, *rotation, charset));    
     } else if mode == "decrypt" || mode == "d" {
-        println!("{}", cypher_encode(&file_content, -(*rotation)));
+        println!("{}", cypher_encode(&file_content, -(*rotation), charset));
     } else {
         println!("ERROR: mode can only be `encrypt` (e) or `decrypt` (d)")
     }
     println!();
 }
 
-fn cypher_encode(input: &str, rotation: isize) -> String {
-    let rotation = rotation as isize % CHARACTERS.len() as isize;
+fn cypher_encode(input: &str, rotation: isize, charset: &String) -> String {
+    let rotation = rotation as isize % charset.len() as isize;
     input
         .chars()
         .map(|ch| {
-            if !CHARACTERS.contains(ch) {
+            if !charset.contains(ch) {
                 ch
             } else {
-                CHARACTERS
+                charset
                     .chars()
                     .nth(
-                        ((CHARACTERS.len() as isize
-                            + (CHARACTERS.find(ch).unwrap() as isize - (rotation)))
-                            % CHARACTERS.len() as isize) as usize,
+                        ((charset.len() as isize
+                            + (charset.find(ch).unwrap() as isize - (rotation)))
+                            % charset.len() as isize) as usize,
                     )
                     .unwrap()
             }
