103bd5ed806e313a8ec60f467964f3a9a1110b90
Aryan Jassal <aryanjassal.au@gmail.com>
2022-08-10 12:19:36 +1000

    properly fixed decoding

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,38 @@
-use std::{
-    env,
-    fs::File,
-    io::{prelude::*, BufReader},
-    path::Path,
-};
+use std::{env, fs};
 
-const CHARACTERS: &str = "ABCDEFGHIJKLMNOPQRSTYUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=!@#$%^&*()_+`~[]{};:\'\"\\|/.,<> ?";
+const CHARACTERS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_ ";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    let rotation = &args[2];
-    let lines = read_from_file(filename);
+    let rotation = &args[2].parse().unwrap_or(1);
+    let file_content = fs::read_to_string(filename).unwrap();
+
+    let encoded_text = cypher_encode(&file_content, *rotation);
 
     println!();
-    lines.iter().for_each(|line| {
-        println!("{:?}", cypher_encode(line, rotation.parse().unwrap_or(1)));
-    });
+    println!("{}", &encoded_text);
     println!();
-    lines.iter().for_each(|line| {
-        println!("{:?}", cypher_decode(cypher_encode(line, 1).as_str(), rotation.parse().unwrap_or(1)));
-    });
+    println!("{}", cypher_encode(&encoded_text, -(*rotation)));
 }
 
-fn read_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("ERROR: file does not exist");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("ERROR: could not parse line"))
-        .collect()
-}
-
-fn cypher_encode(input: &str, rotation: usize) -> String {
-    let mut buffer = String::new();
-
-    input.chars().for_each(|ch| {
-        if !CHARACTERS.contains(ch) {
-            buffer.push(ch);
-        } else {
-            buffer.push(
+fn cypher_encode(input: &str, rotation: isize) -> String {
+    let rotation = rotation as isize % CHARACTERS.len() as isize;
+    input
+        .chars()
+        .map(|ch| {
+            if !CHARACTERS.contains(ch) {
+                ch
+            } else {
                 CHARACTERS
                     .chars()
-                    .nth((CHARACTERS.find(ch).unwrap() + rotation) % (CHARACTERS.len() - 1))
-                    .unwrap(),
-            );
-        }
-    });
-    buffer
-}
-
-fn cypher_decode(input: &str, rotation: usize) -> String {
-    let mut buffer = String::new();
-
-    input.chars().for_each(|ch| {
-        if !CHARACTERS.contains(ch) {
-            buffer.push(ch);
-        } else {
-            buffer.push(
-                CHARACTERS
-                    .chars()
-                    .nth((CHARACTERS.find(ch).unwrap() + CHARACTERS.len() - rotation - 1) % (CHARACTERS.len() - 1))
-                    .unwrap(),
-            );
-        }
-    });
-    buffer
+                    .nth(
+                        ((CHARACTERS.len() as isize
+                            + (CHARACTERS.find(ch).unwrap() as isize - (rotation)))
+                            % CHARACTERS.len() as isize) as usize,
+                    )
+                    .unwrap()
+            }
+        })
+        .collect::<String>()
 }
